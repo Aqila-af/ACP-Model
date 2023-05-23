6c0219621d96ce601ac5c962ae18d46b2318c72b
Aqila <aqela.af@gmail.com>
2023-05-24 00:08:11 +0200

    defined the measures

--- subscriptionService.pm
+++ subscriptionService.pm
@@ -29,21 +29,6 @@ species S;       /* Customer satisfaction */
 const l = 1000;       /* Number of max potential customers */
 const n = 100;       /* Number of max active customers */   
 
-/* _-_-_-_-_-_-_-_-_-_-_ Measures _-_-_-_-_-_-_-_-_-_-_ */
-
-
-/* Total number of customers over time */
-measure total_customers = initial_customers + #acquired_customers - #churned_customers;
-
-/* Number of acquired customers */
-measure acquired_customers = acquisition_rate * time;
-
-/* Number of churned customers */
-measure churned_customers = churn_rate * time;
-
-/* Customer satisfaction */
-measure satisfaction = satisfaction_rate * time;
-
 
 param marketingCampigns = 0.7;
 param promotion = 0.4;
@@ -71,5 +56,22 @@ rule CustomerSatisfaction for i in [1, n]{
         S|A[i] -[ #S *%A * (serviceQuality * satisfaction_rate) ]-> S[i]|A[i]
 
 }
+
+
+/* _-_-_-_-_-_-_-_-_-_-_ Measures _-_-_-_-_-_-_-_-_-_-_ */
+
+
+/* Total number of customers over time */
+measure total_customers = initial_customers + #acquired_customers - #churned_customers;
+
+/* Number of acquired customers */
+measure acquired_customers = acquisition_rate * time;
+
+/* Number of churned customers */
+measure churned_customers = churn_rate * time;
+
+/* Customer satisfaction */
+measure satisfaction = satisfaction_rate * time;
+
 
 system init = A<1000> C<30> P<100> S<60>;
