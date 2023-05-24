64d40596f9674350b639e76a1aaf3c89a0f4fdf0
Aqila <aqela.af@gmail.com>
2023-05-24 12:34:15 +0200

    defined the new  parameters and modified the rules

--- subscriptionService.pm
+++ subscriptionService.pm
@@ -4,50 +4,44 @@
 /* _-_-_-_-_-_-_-_-_-_-_ Params _-_-_-_-_-_-_-_-_-_-_ */
 
 
-/* Total number of customers at the beginning of the analysis */
-param initial_customers = 1000;
+param potential_customers = 1000;
 
-/* Average monthly customer acquisition rate */
-param acquisition_rate = 50;
 
-/* Average monthly customer churn rate */
-param churn_rate = 30;
+/* Rate of acquiring new customers */
+param acquisition_rate = 0.2;
 
-/* Average monthly customer satisfaction rate (between 0 and 1) */
-param satisfaction_rate = 0.8;
-
-/* _-_-_-_-_-_-_-_-_-_-_ Species _-_-_-_-_-_-_-_-_-_-_ */
-
-species A;   /* Active customers */
-species C;    /* Churned customers */
-species P;     /* Potential customers */
-species S;       /* Customer satisfaction */
-
+/* Rate of churn (customers leaving the service) */
+param churn_rate = 0.1;
 
 /* _-_-_-_-_-_-_-_-_-_-_ Consts _-_-_-_-_-_-_-_-_-_-_ */
 
-const l = 1000;       /* Number of max potential customers */
-const n = 100;       /* Number of max active customers */   
+const max_cutomers = 1000;
 
+const max_marketing_strategies = 5;
 
-param marketingCampigns = 0.7;
-param promotion = 0.4;
+const daily_marketing_emails = 1000;
 
-param serviceQuality = 0.8;
-param lackOfValue = 0.3;
-param competition = 0.8;
+const daily_targeted_emails = 100;
 
+const useof_MachineLearning tools = 1;
 
+/* _-_-_-_-_-_-_-_-_-_-_ Species _-_-_-_-_-_-_-_-_-_-_ */
 
-rule CustomerAcquisition for i in [1, l]{
+species A of [0, max_customers];    /* Acquired customers */
+species C;    /* Churned customers */
 
-        P[i] -[ #P * (marketingCampigns * promotion *acquisition_rate) ]-> A[i]|P[l-1]
+/* _-_-_-_-_-_-_-_-_-_-_ Rules _-_-_-_-_-_-_-_-_-_-_ */
+
+rule CustomerAcquisition for i in [0, potential_customers]{
+
+        A[i] -[ initial_customers * acquisition_rate ]-> A[i]
+
 
 }
 
-rule CustomerChurn for i in [1, n]{
+rule CustomerChurn for i in [0, max_customers]{
 
-        A[i] -[ #A * (serviceQuality * lackOfValue * competition * churn_rate * (1 - satisfaction_rate) ) ]-> C[i]|A[n-1]
+        A[i] -[ #A  * churn_rate]-> C[i]|A[n-1]
 
 }
 
@@ -58,14 +52,15 @@ rule CustomerChurn for i in [1, n]{
 /* Total number of customers over time */
 measure total_customers = initial_customers + #acquired_customers - #churned_customers;
 
-/* Number of acquired customers */
-measure acquired_customers = acquisition_rate * time;
+/* Total number of aquired customers over time */
+measure acquired_customers = #A;
 
-/* Number of churned customers */
-measure churned_customers = churn_rate * time;
-
-/* Customer satisfaction */
-measure satisfaction = satisfaction_rate * time;
+/* Total number of churned customers over time */
+measure churned_customers = #C;
 
 
-system init = A<1000> C<30> P<100> S<60>;
+
+/* _-_-_-_-_-_-_-_-_-_-_ System Initial values_-_-_-_-_-_-_-_-_-_-_ */
+
+
+system init = P<potential_customers>;
